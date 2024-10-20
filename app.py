9c1acb320893bd3837852d24d2bcee764cd6c12c
SatvikTripathi <87223730+SatvikTripathi@users.noreply.github.com>
2024-10-20 16:31:46 +0530

    Added host

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ def recommend():
     return render_template("recommend.html", data = suggestions)
 
 if __name__ == "__main__":
-    app.run(debug = True , port=80)
+    app.run(host="0.0.0.0",debug = True , port=80)
 
 
 
